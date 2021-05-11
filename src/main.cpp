3a492841bc8087e140c91d38dc1ccf4e1a8cc93b
Trevor Pilgrim <pilg5971@stthomas.edu>
2021-05-11 13:42:00 -0500

    Loaded Textures + Displays current Path

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <cstring>
 #include <vector>
 #include <sys/stat.h>
@@ -14,12 +16,27 @@ class File {
         bool is_dir;
 };
 
+typedef struct AppData {
+    TTF_Font *font;
+    SDL_Texture *Directory;
+    SDL_Texture *Executable;
+    SDL_Texture *Image;
+    SDL_Texture *Video;
+    SDL_Texture *Code;
+    SDL_Texture *Other;
+    std::string PathText;
+    SDL_Texture *Path;
+    SDL_Rect Icon_rect;
+    SDL_Rect Path_rect;
+    SDL_Rect Path_container;
+} AppData;
 
-void initialize(SDL_Renderer *renderer);
-void render(SDL_Renderer *renderer);
+void initialize(SDL_Renderer *renderer, AppData *data);
+void render(SDL_Renderer *renderer, AppData *data);
 
 std::vector<File*> getItemsInDirectory(std::string dirpath);
 void freeItemVector(std::vector<File*> *vector_ptr);
+void setPath(AppData *data, std::string path);
 
 int main(int argc, char **argv)
 {
@@ -28,6 +45,8 @@ int main(int argc, char **argv)
 
     // initializing SDL as Video
     SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+    TTF_Init();
 
     // create window and renderer
     SDL_Renderer *renderer;
@@ -47,9 +66,14 @@ int main(int argc, char **argv)
     freeItemVector(&file_v);
     // ------------------------------------------------------------------
 
+
+    // Initializing AppData----------------------------------------------
+    AppData data;
+    setPath(&data, std::string(home));
+
     // initialize and perform rendering loop
-    initialize(renderer);
-    render(renderer);
+    initialize(renderer, &data);
+    render(renderer, &data);
     SDL_Event event;
     SDL_WaitEvent(&event);
     while (event.type != SDL_QUIT)
@@ -61,23 +85,69 @@ int main(int argc, char **argv)
     // clean up
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
+    IMG_Quit();
     SDL_Quit();
 
     return 0;
 }
 
-void initialize(SDL_Renderer *renderer)
+void initialize(SDL_Renderer *renderer, AppData *data)
 {
     // set color of background when erasing frame
     SDL_SetRenderDrawColor(renderer, 235, 235, 235, 255);
+
+    /*-----------------------Initializing Textures-----------------------*/
+    SDL_Surface *img_surf = IMG_Load("resrc/File Icons/DirectoryIcon.png");
+    data->Directory = SDL_CreateTextureFromSurface(renderer, img_surf);
+    SDL_FreeSurface(img_surf);
+
+    SDL_Surface *img2_surf = IMG_Load("resrc/File Icons/ExeIcon.png");
+    data->Executable = SDL_CreateTextureFromSurface(renderer, img2_surf);
+    SDL_FreeSurface(img2_surf);
+    
+    SDL_Surface *img3_surf = IMG_Load("resrc/File Icons/ImageIcon.png");
+    data->Image = SDL_CreateTextureFromSurface(renderer, img3_surf);
+    SDL_FreeSurface(img3_surf);
+    
+    SDL_Surface *img4_surf = IMG_Load("resrc/File Icons/VideoIcon.png");
+    data->Video = SDL_CreateTextureFromSurface(renderer, img4_surf);
+    SDL_FreeSurface(img4_surf);
+    
+    SDL_Surface *img5_surf = IMG_Load("resrc/File Icons/CodeIcon.png");
+    data->Code = SDL_CreateTextureFromSurface(renderer, img5_surf);
+    SDL_FreeSurface(img5_surf);
+    
+    SDL_Surface *img6_surf = IMG_Load("resrc/File Icons/OtherIcon.png");
+    data->Other = SDL_CreateTextureFromSurface(renderer, img6_surf);
+    SDL_FreeSurface(img6_surf);
+    
+    /*-----------------------Initializing Path Text------------------------*/
+    data->font = TTF_OpenFont("resrc/OpenSans-Regular.ttf", 24);
+    SDL_Color color = {0, 0, 0};
+    SDL_Surface *path_surface = TTF_RenderText_Solid(data->font, data->PathText.c_str(), color);
+    data->Path = SDL_CreateTextureFromSurface(renderer, path_surface);
+    SDL_FreeSurface(path_surface);
+
+
+    /*-----------------------Initializing Rectangles----------------------*/
+    // My_rect = {x, y, width, height}
+    
+    data->Path_container = {30, 30, 200, 150};
+
+    data->Path_rect = {50, 50, 100, 100};
+    SDL_QueryTexture(data->Path, NULL, NULL, &(data->Path_rect.w), &(data->Path_rect.h));
+    
+    data->Icon_rect = {50, 250, 50, 50};
 }
 
-void render(SDL_Renderer *renderer)
-{
+void render(SDL_Renderer *renderer, AppData *data){
     // erase renderer content
     SDL_RenderClear(renderer);
     
     // TODO: draw!
+    SDL_RenderCopy(renderer, data->Path, NULL, &(data->Path_rect));
+    SDL_RenderCopy(renderer, data->Other, NULL, &(data->Icon_rect));
 
     // show rendered frame
     SDL_RenderPresent(renderer);
@@ -118,4 +188,8 @@ void freeItemVector(std::vector<File*> *vector_ptr)
         File* fp = vector_ptr->at(i);
         delete fp;
     }
+}
+
+void setPath(AppData *data, std::string path){
+    data->PathText = path;
 }
