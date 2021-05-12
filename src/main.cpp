33a4f987a9beb809bb6c145ea7491216589b756e
Cole Potter <pott6366@stthomas.edu>
2021-05-11 21:38:38 -0500

    Added scrolling and expanded File class with Size, Extension, and Permission strings

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <cstring>
+#include <string.h>
 #include <vector>
 #include <sys/stat.h>
 #include <dirent.h>
@@ -14,6 +15,9 @@ class File {
     public:
         std::string name;
         bool is_dir;
+        std::string extension;
+        std::string permissions;
+        std::string size;
 };
 
 typedef struct AppData {
@@ -35,13 +39,19 @@ typedef struct AppData {
     SDL_Rect Path_rect;
     SDL_Rect Path_container;
 
+    int scroll_offset;
 } AppData;
 
 void initialize(SDL_Renderer *renderer, AppData *data);
 void render(SDL_Renderer *renderer, AppData *data, std::vector<File*> files);
 
+void resetRenderData(AppData *data);
+
 std::vector<File*> getItemsInDirectory(std::string dirpath);
 void freeItemVector(std::vector<File*> *vector_ptr);
+std::string parsePermission(mode_t permission_mode);
+std::string parseSize(size_t byte_size);
+
 void setPath(AppData *data, std::string path);
 void renderFiles(SDL_Renderer *renderer, AppData *data, std::vector<File*> files);
 
@@ -60,24 +70,11 @@ int main(int argc, char **argv)
     SDL_Window *window;
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 
-    // ! EXAMPLE --------------------------------------------------------
-    // log home dir contents to console
-    std::vector<File*> file_v = getItemsInDirectory(home);
-    for(int i = 0; i < file_v.size(); i++) {
-        if(file_v[i]->is_dir) {
-            printf("\x1b[34m%s/\x1b[0m\n",file_v[i]->name.c_str());
-        } else {
-            printf("%s\n",file_v[i]->name.c_str());
-        }
-    }
-    freeItemVector(&file_v);
-    // ------------------------------------------------------------------
-
-
     // Initializing AppData----------------------------------------------
     AppData data;
     setPath(&data, std::string(home));
     std::vector<File*> files = getItemsInDirectory(home);
+    data.scroll_offset = 0;
 
     // initialize and perform rendering loop
     initialize(renderer, &data);
@@ -86,7 +83,15 @@ int main(int argc, char **argv)
     SDL_WaitEvent(&event);
     while (event.type != SDL_QUIT)
     {
-        //render(renderer);
+        if (event.type == SDL_MOUSEWHEEL)
+        {
+            data.scroll_offset += event.wheel.y << 4;
+            // Don't allow to scroll above files (offset inverted)
+            if(data.scroll_offset > 0) data.scroll_offset = 0;
+        }
+
+        resetRenderData(&data);
+        render(renderer, &data, files);
         SDL_WaitEvent(&event);
     }
 
@@ -97,10 +102,14 @@ int main(int argc, char **argv)
     IMG_Quit();
     SDL_Quit();
 
-    delete(&data);
-    delete(&files);
+    freeItemVector(&files);
 
     return 0;
+}
+
+// Reset any data needed for a render update
+void resetRenderData(AppData *data) {
+    data->Icon_rect = {20, 60 + data->scroll_offset, 30, 30};
 }
 
 void initialize(SDL_Renderer *renderer, AppData *data)
@@ -154,6 +163,7 @@ void initialize(SDL_Renderer *renderer, AppData *data)
 
 void render(SDL_Renderer *renderer, AppData *data, std::vector<File*> files){
     // erase renderer content
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0);
     SDL_RenderClear(renderer);
     
     // TODO: draw!
@@ -177,18 +187,40 @@ void render(SDL_Renderer *renderer, AppData *data, std::vector<File*> files){
 std::vector<File*> getItemsInDirectory(std::string dirpath)
 {
     std::vector<File*> file_vector;
-    struct stat info;
-    int err = stat(dirpath.c_str(), &info);
-    if(err == 0 && S_ISDIR(info.st_mode))
+    struct stat dir_info;
+    int err = stat(dirpath.c_str(), &dir_info);
+    if(err == 0 && S_ISDIR(dir_info.st_mode))
     {
         DIR* dir = opendir(dirpath.c_str());
         struct dirent *entry;
+        struct stat entry_info;
+        std::string full_path;
+
+        int dot_pos;
         
         while((entry = readdir(dir)) != NULL)
         {
             File* file_entry = new File();
             file_entry->name = entry->d_name;
             file_entry->is_dir = (entry->d_type == DT_DIR);
+            // get file stat
+            full_path = dirpath + "/" + file_entry->name;
+            stat(full_path.c_str(), &entry_info);
+            // extract extension
+            // if a . is found
+            if((dot_pos = file_entry->name.find_last_of('.')) != file_entry->name.npos) {
+                // set the extension to every character after the .
+                file_entry->extension = file_entry->name;
+                file_entry->extension = file_entry->extension.erase(0, dot_pos+1);
+            }
+
+            // extract permissions
+            file_entry->permissions = parsePermission(entry_info.st_mode);
+            
+            // extract size
+            file_entry->size = parseSize(entry_info.st_size);
+
+            //printf("%-40s - %s\n", file_entry->name.c_str(), file_entry->size.c_str());
             file_vector.push_back(file_entry);
         }
     }
@@ -205,6 +237,102 @@ void freeItemVector(std::vector<File*> *vector_ptr)
         File* fp = vector_ptr->at(i);
         delete fp;
     }
+}
+
+/** Parses permissions mode into permission string
+ * @param permission_mode mode_t permission format
+ * @return A string of permissions
+ */
+std::string parsePermission(mode_t permission_mode)
+{
+    std::string permission_string = "";
+    // if it's a d, push d
+    if(S_ISDIR(permission_mode))
+    {
+        permission_string.push_back('d');
+    } else {
+        permission_string.push_back('-');
+    }
+
+    int shift_root;
+    for(int i = 2; i >= 0; i--) {
+        shift_root = i * 3;
+        if (permission_mode & (0x1 << (shift_root + 2)))
+        {
+            permission_string.push_back('r');
+        } else {
+            permission_string.push_back('-');
+        }
+        if (permission_mode & (0x1 << (shift_root + 1)))
+        {
+            permission_string.push_back('w');
+        } else {
+            permission_string.push_back('-');
+        }
+        if (permission_mode & (0x1 << (shift_root)))
+        {
+            permission_string.push_back('x');
+        } else {
+            permission_string.push_back('-');
+        }
+    }
+    return permission_string;
+}
+
+/** Parse size in bytes into human readable format
+ * @param byte_size size of file in bytes
+ * @return Human readable size string
+ */
+std::string parseSize(size_t byte_size) {
+    std::string size_str = "";
+    int den, pre, pos;
+    
+    // GiB
+    if(byte_size >> 30) {
+        den = 1 << 30;
+        pre = byte_size / den;
+        pos = (byte_size % den) / (den / 10);
+        size_str.append(std::to_string(pre));
+        if(pos != 0)
+        {
+            size_str.push_back('.');
+            size_str.append(std::to_string(pos));
+        }
+        size_str.append(" GiB");
+    }
+    // MiB
+    else if (byte_size >> 20) {
+        den = 1 << 20;
+        pre = byte_size / den;
+        pos = (byte_size % den) / (den / 10);
+        size_str.append(std::to_string(pre));
+        if(pos != 0)
+        {
+            size_str.push_back('.');
+            size_str.append(std::to_string(pos));
+        }
+        size_str.append(" MiB");
+    }
+    // KiB
+    else if (byte_size >> 10) {
+        den = 1 << 10;
+        pre = byte_size / den;
+        pos = (byte_size % den) / (den / 10);
+        size_str.append(std::to_string(pre));
+        if(pos != 0)
+        {
+            size_str.push_back('.');
+            size_str.append(std::to_string(pos));
+        }
+        size_str.append(" KiB");
+    }
+    // B
+    else {
+        size_str.append(std::to_string(byte_size));
+        size_str.append(" B");
+    }
+
+    return size_str;
 }
 
 void setPath(AppData *data, std::string path){
